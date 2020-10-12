ddbec8082613ed9aef73e8f1f28604eeff2ac926
flomi <flomikoon@gmail.com>
2020-10-12 23:39:43 +0300

    lab2 Task4

--- labs/02/continuewatch/app/src/main/java/ru/spbstu/icc/kspt/lab2/continuewatch/MainActivity.kt
+++ labs/02/continuewatch/app/src/main/java/ru/spbstu/icc/kspt/lab2/continuewatch/MainActivity.kt
@@ -1,13 +1,19 @@
 package ru.spbstu.icc.kspt.lab2.continuewatch
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     var secondsElapsed: Int = 0
 
-    var backgroundThread = Thread {
+    private lateinit var mSettings: SharedPreferences
+
+
+    private var backgroundThread = Thread {
         while (true) {
             Thread.sleep(1000)
             textSecondsElapsed.post {
@@ -19,6 +25,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mSettings=getSharedPreferences("myPref", Context.MODE_PRIVATE)
         backgroundThread.start()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        secondsElapsed=mSettings.getInt("seconds",secondsElapsed);
+    }
+
+    override fun onPause() {
+        super.onPause()
+        val editor = mSettings.edit()
+        editor.putInt("seconds",secondsElapsed)
+        editor.apply()
     }
 }
